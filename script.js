77eeb659d07faeea4d3a107ae47d8b5ec987f6c4
amitjait <amit.rimit591@gmail.com>
2023-02-15 01:12:04 +0530

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 //your code here
+
+let pageNumberHere = 1;
+async function fetchData(){
+    let url = `https://api.github.com/repositories/1296269/issues?page=${pageNumberHere}&per_page=5`;
+    const result = await fetch(url);
+    const data = await result.json();
+
+    let pageNumber = document.getElementById('page-number');
+    pageNumber.innerHTML = "Page Number " + pageNumberHere;
+
+    let ol = document.createElement('ol');
+
+    data.map((issue) =>{
+        ol.innerHTML += `<li>${issue.title}</li>`;
+    })
+
+    let dataDiv =  document.getElementById('issues');
+    dataDiv.innerHTML = "";
+    dataDiv.appendChild(ol);
+}
+
+fetchData();
+
+function next(){
+    pageNumberHere++;
+
+    let prevBtn = document.getElementById('load_prev');
+
+    if(pageNumberHere > 1){
+        prevBtn.disabled = false;
+    }
+
+    fetchData();
+}
+
+function prev(){
+
+    pageNumberHere--;
+    let prevBtn = document.getElementById('load_prev');
+
+    if(pageNumberHere == 1){
+        prevBtn.disabled = true;
+    }
+    fetchData();
+}
+
+
